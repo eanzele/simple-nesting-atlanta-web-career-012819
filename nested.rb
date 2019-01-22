7110ad86232f65f520c285909d94a57a40c28e87
Emmanuel Anzele <eanzele@gmail.com>
2019-01-22 07:47:26 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -66,7 +66,7 @@ def adding_matz
 # }
 
 		programmer_hash[:yukihiro_matsumoto] = {known_for: "Ruby", languages: ["LISP, C"]}
-
+end
 	programmer_hash = 
  		{
         :grace_hopper => {
@@ -104,7 +104,7 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     programmer_hash[:alan_kay][:known_for] = "GUI"
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
      
 end
 
